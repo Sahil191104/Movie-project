4f445c57d4ba07edbbb6c130aba7de18d06f4573
Sahil191104 <sahilrupareliya95@gmail.com>
2023-05-22 00:11:10 +0545

    22_5_2023

--- assets/js/adminMovie.js
+++ assets/js/adminMovie.js
@@ -10,6 +10,9 @@ const Movie = () => {
     event.preventDefault();
 }
 
+let update = false;
+let id = null;
+
 let MovieformRef = document.getElementById("Movie_form");
 
 const Movieonload = () => {
@@ -26,6 +29,17 @@ const Movieonload = () => {
 
         optionref.appendChild(optionEle);
     });
+};
+
+const handleLoad = () => {
+    let localData = JSON.parse(localStorage.getItem("Movie"));
+
+    if (localData) {
+        localData.map((value) => {
+            displayData(value.id, value.name, value.desc, value.time, value.poster);
+            document.getElementById("FormMovie1").style.display = "block";
+        });
+    };
 };
 
 const addbtn = () => {
@@ -71,6 +85,77 @@ const removebtn = (randomMo) => {
     event.preventDefault();
 }
 
+const displayData = (randomMo, Movieame, MovieDiscription, MovieTime, MovieFile) => {
+
+    let timeForm = document.getElementById("tablebody");
+
+    let tr = document.createElement("tr");
+    tr.setAttribute("id", "row" + randomMo);
+
+    let td1 = document.createElement("td");
+    let td1Text = document.createTextNode(Movieame);
+    td1.appendChild(td1Text);
+    tr.appendChild(td1);
+
+    let td2 = document.createElement("td");
+    let td2Text = document.createTextNode(MovieDiscription);
+    td2.appendChild(td2Text);
+    tr.appendChild(td2);
+
+    let td3 = document.createElement("td");
+    let td3Text = document.createTextNode(MovieTime);
+    td3.appendChild(td3Text);
+    tr.appendChild(td3);
+
+    // let td4 = document.createElement("td");
+    // let td4Text = document.createTextNode(time);
+    // td4.appendChild(td4Text);
+    // tr.appendChild(td4);
+
+    let td5 = document.createElement("td");
+    let img = document.createElement("img");
+    img.setAttribute("src", MovieFile);
+    td5.appendChild(img);
+    tr.appendChild(td5);
+
+    let td6 = document.createElement("td");
+
+    let td6div = document.createElement("div");
+    td6div.setAttribute("id", "divbtn");
+
+    let i1 = document.createElement("button");
+    i1.setAttribute("onclick", "dataEdit(" + randomMo + ")");
+    i1.setAttribute("id", "btnadmin1");
+    let btn1 = document.createTextNode("Edit");
+    i1.appendChild(btn1);
+    td6div.appendChild(i1);
+    td6.appendChild(td6div);
+
+    let i2 = document.createElement("button");
+    i2.setAttribute("onclick", "dataRemove(" + randomMo + ")");
+    i2.setAttribute("id", "btnadmin1");
+    let btn2 = document.createTextNode("Delete");
+    i2.appendChild(btn2);
+    td6div.appendChild(i2);
+    td6.appendChild(td6div);
+    tr.appendChild(td6);
+
+    timeForm.appendChild(tr);
+}
+
+const dataRemove = (randomMo) => {
+    let localData = JSON.parse(localStorage.getItem("Movie"));
+    let tr = document.getElementById("row" + randomMo);
+    tr.remove();
+
+    localData.map((value, index) => {
+        if (value.id === randomMo) {
+            localData.splice(index, 1);
+            localStorage.setItem("Movie", JSON.stringify(localData));
+        };
+    });
+};
+
 const handleMovie = () => {
     console.log("scdc");
     let Movieame =document.getElementById("moname").value;
@@ -80,17 +165,17 @@ const handleMovie = () => {
 
     let randomMo = Math.floor(Math.random() * 1000);
 
-    let localDatamovie = JSON.parse(localStorage.getItem("Movie"));
+    let localData = JSON.parse(localStorage.getItem("Movie"));
 
-    if (localDatamovie) {
-        localDatamovie.push({
+    if (localData) {
+        localData.push({
             id: randomMo,
             name: Movieame,
             desc:MovieDiscription,
             time:[MovieTime],
             poster:MovieFile
         });
-        localStorage.setItem("Movie", JSON.stringify(localDatamovie));
+        localStorage.setItem("Movie", JSON.stringify(localData));
     } else {
         localStorage.setItem("Movie", JSON.stringify([{
             id: randomMo,
@@ -100,9 +185,10 @@ const handleMovie = () => {
             poster:MovieFile
         }]));
     };
-
+    displayData(id, name, desc, time, poster);
     event.preventDefault();
 }
 
 window.onload = Movieonload();
+window.onload = handleLoad();
 MovieformRef.addEventListener("submit", handleMovie);
