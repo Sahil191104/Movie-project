b203345bd8e442bf2b37e4c9fcd62fe71d3922d7
Sahil191104 <sahilrupareliya95@gmail.com>
2023-05-30 21:04:47 +0545

    30_5_2023

--- assets/js/DisplayCinemadata.js
+++ assets/js/DisplayCinemadata.js
@@ -3,18 +3,32 @@ const handleOnload = () => {
     let localdata = JSON.parse(localStorage.getItem("cinema"));
     let sessiondata = JSON.parse(sessionStorage.getItem("mname"));
 
-    let cinemaData = localData.filter((value, i) => value.name === sessiondata);
+    let movieData = localData.filter((value, i) => value.name === sessiondata);
+    let temp = [];
+    let temp1 = [];
 
-    console.log(cinemaData);
-    print = '<div class="row">';
-    cinemaData.map((value) => {
-        let temp = [];
-        temp = value.id;
+    movieData.map((value) => {
+        temp.push(localdata);
         console.log(temp);
-    });
-    print += '</div>';
 
-    document.getElementById("displaydatacinama").innerHTML = print;
+        temp.map((value) => {
+            temp1.push(temp);
+            console.log(temp1);
+            let localdata = JSON.parse(localStorage.getItem("cinema"));
+            print = '<div class="row">';
+            temp1.map((value) => {
+                print += '<div class="col-lg-3 col-2" id="col' + value.id + ' ">';
+                print += '<div class="card bg-body-tertiary">';
+                print += '<div class="card-body">';
+                print += '<div class="flexdiv">' + '<h6 class="card-title">Cinema Name: </h6>' + '<span>' + value.name + '</span>' + '</div>';
+                print += '<div class="flexdiv">' + '<h6 class="card-title">Cinema Location: </h6>' + '<span>' + value.locaton + '</span>' + '</div>';
+                print += '<div class="flexdiv">' + '<h6 class="card-title">Cinema Facility: </h6>' + '<span>' + value.facilites + '</span>' + '</div>';
+                print += '</div></div></div>';
+            });
+            print += '</div>';
+        });
+        document.getElementById("displaydatacinama").innerHTML = print;
+    });
 };
 
 window.onload = handleOnload();
