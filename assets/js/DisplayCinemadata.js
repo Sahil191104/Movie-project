31476e474be6240b8891e33a5d1db36c6674bc33
Sahil191104 <sahilrupareliya95@gmail.com>
2023-06-01 22:26:59 +0545

    1_6_2023

--- assets/js/DisplayCinemadata.js
+++ assets/js/DisplayCinemadata.js
@@ -1,31 +1,35 @@
 const handleOnload = () => {
     let localData = JSON.parse(localStorage.getItem("Movie"));
-    let localdata = JSON.parse(localStorage.getItem("cinema"));
     let sessiondata = JSON.parse(sessionStorage.getItem("mname"));
 
-    let movieData = localData.filter((value, i) => value.name === sessiondata);
+    let movieData = localData.filter((value, i) => sessiondata === value.name);
     let temp = [];
-    let temp1 = [];
 
     movieData.map((value) => {
-        temp.push(localdata);
+        temp.push(value.cid);
         console.log(temp);
 
-        temp.map((value) => {
-            temp1.push(temp);
-            console.log(temp1);
-            print = '<div class="row">';
-            temp1.map((value) => {
-                print += '<div class="col-lg-3 col-2" id="col' + value.id + ' ">';
-                print += '<div class="card bg-body-tertiary">';
-                print += '<div class="card-body">';
-                print += '<div class="flexdiv">' + '<h6 class="card-title">Cinema Name: </h6>' + '<span>' + value.name + '</span>' + '</div>';
-                print += '<div class="flexdiv">' + '<h6 class="card-title">Cinema Location: </h6>' + '<span>' + value.locaton + '</span>' + '</div>';
-                print += '<div class="flexdiv">' + '<h6 class="card-title">Cinema Facility: </h6>' + '<span>' + value.facilites + '</span>' + '</div>';
-                print += '</div></div></div>';
-            });
-            print += '</div>';
+        let localdata = JSON.parse(localStorage.getItem("cinema"));
+        print = '<div class="row">';
+        localdata.map((value) => {
+            for (let i = 0; i < temp.length; i++) {
+                // console.log(temp.length);
+                // console.log(value.id);
+                // console.log(parseInt(temp[i]));
+                if (value.id === parseInt(temp[i])) {
+                    console.log("vfvf");
+                    print += '<div class="col-lg-3 col-2">';
+                    print += '<div class="card bg-body-tertiary">';
+                    print += '<div class="card-body">';
+                    print += '<div class="flexdiv">' + '<h6 class="card-title">Cinema Name: </h6>' + '<span>' + value.name + '</span>' + '</div>';
+                    print += '<div class="flexdiv">' + '<h6 class="card-title">Cinema Location: </h6>' + '<span>' + value.locaton + '</span>' + '</div>';
+                    print += '<div class="flexdiv">' + '<h6 class="card-title">Cinema Facility: </h6>' + '<span>' + value.facilites + '</span>' + '</div>';
+                    print += '</div></div></div>';
+                };
+            };
         });
+        print += '</div>';
+        console.log(print);
         document.getElementById("displaydatacinama").innerHTML = print;
     });
 };
